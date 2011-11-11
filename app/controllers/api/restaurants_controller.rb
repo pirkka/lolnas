99adfd925ffa493cc8ec337258aee1d0a2c71dbf
Pirkka Esko <pirkka@gmail.com>
2011-11-11 14:43:51 +0200

    distance sorting to API

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,8 +1,22 @@
 class Api::RestaurantsController < Api::ApiController
 
   def index
-    @restaurants = Restaurant.all
+    # defaults
+    @latitude = 60.172389
+    @longitude = 24.947516
+    
+    if params[:latitude]
+      @latitude = params[:latitude]
+    end
 
+    if params[:longitude]
+      @longitude = params[:longitude]
+    end
+    
+    # sort by distance
+    @restaurants = Restaurant.all
+    @restaurants = Restaurant.sort_by_distance(@restaurants, @latitude, @longitude)
+    
     respond_with(@restaurants, :api_template => :valid_lunches_only)
   end
 
