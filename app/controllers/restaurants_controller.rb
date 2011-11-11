f7582b4804bc0d2c7ef3eaef2250005a078339fd
Olli Jokinen <olli.jokinen@enemy.fi>
2011-11-11 15:05:42 +0200

    latitude & longitude floattina

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,15 +4,10 @@ class RestaurantsController < ApplicationController
     # defaults
     @latitude = 60.172389
     @longitude = 24.947516
-    
-    if params[:latitude]
-      @latitude = params[:latitude]
-    end
 
-    if params[:longitude]
-      @longitude = params[:longitude]
-    end
-    
+    @latitude = params[:latitude].to_f if params[:latitude]
+    @longitude = params[:longitude].to_f if params[:longitude]
+
     # sort by distance
     @restaurants = Restaurant.all
     @restaurants = Restaurant.sort_by_distance(@restaurants, @latitude, @longitude)
