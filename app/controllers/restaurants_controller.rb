00bd880064fa0f1f1acfd6dc19b5dfcf37951d28
Pirkka Esko <pirkka@gmail.com>
2011-11-11 14:37:14 +0200

    not showing distance lat & lon parameter support

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,14 @@ class RestaurantsController < ApplicationController
     @latitude = 60.172389
     @longitude = 24.947516
     
+    if params[:latitude]
+      @latitude = params[:latitude]
+    end
+
+    if params[:longitude]
+      @longitude = params[:longitude]
+    end
+    
     # sort by distance
     @restaurants = Restaurant.all
     @restaurants = Restaurant.sort_by_distance(@restaurants, @latitude, @longitude)
