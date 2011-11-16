8f642a0e6f01236cb162e651b25730c21e620a6b
Olli Jokinen <olli.jokinen@enemy.fi>
2011-11-16 17:50:23 +0200

    sendgrid settings for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,4 +59,15 @@ Lolnas::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'www.lolnas.fi' }
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+
+  config.action_mailer.delivery_method = :smtp
 end
