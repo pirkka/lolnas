da6dfc933fb92ac9216b0eebb067f124ff05bcce
Olli Jokinen <olli.jokinen@enemy.fi>
2011-11-16 18:02:55 +0200

    fixed confirmation controller

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,15 +1,15 @@
 class ConfirmationsController < Devise::ConfirmationsController
   def show
-    @api_user = ApiUser.find_by_confirmation_token(params[:confirmation_token])
+    @api_user = Api::User.find_by_confirmation_token(params[:confirmation_token])
     unless @api_user.present?
       redirect_to root_path
     end
   end
 
   def confirm_account
-    @api_user = ApiUser.find_by_confirmation_token(params[:api_user][:confirmation_token])
+    @api_user = Api::User.find_by_confirmation_token(params[:api_user][:confirmation_token])
     if @api_user.update_attributes(params[:api_user])
-      @api_user = ApiUser.confirm_by_token(@api_user.confirmation_token)
+      @api_user = Api::User.confirm_by_token(@api_user.confirmation_token)
       set_flash_message :notice, :confirmed
       redirect_to root_path
     else
