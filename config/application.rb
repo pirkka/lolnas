014ecab47266a82c2cd3a11d78e92cb096cf7c9d
Olli Jokinen <olli.jokinen@enemy.fi>
2011-11-16 18:15:39 +0200

    i18n fallback fix

--- config/application.rb
+++ config/application.rb
@@ -33,9 +33,6 @@ module Lolnas
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fi
 
-    # rails will fallback to config.i18n.default_locale translation
-    config.i18n.fallbacks = true
-
     # rails will fallback to en, no matter what is set as config.i18n.default_locale
     config.i18n.fallbacks = [:en]
 
