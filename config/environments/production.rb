24c17c90174bcd2fb5e9810b47dfb3e5be48637d
Olli Jokinen <olli.jokinen@enemy.fi>
2011-12-12 18:20:39 +0200

    heroku test #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,12 @@ Lolnas::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.assets.precompile += %w[active_admin.css active_admin.js]
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
