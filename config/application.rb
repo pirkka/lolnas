2b80dc6dd9d2d2fcaeed705a8b294d7022121867
Olli Jokinen <olli.jokinen@enemy.fi>
2011-12-12 18:28:25 +0200

    fixed asset precompile @ heroku

--- config/application.rb
+++ config/application.rb
@@ -48,6 +48,6 @@ module Lolnas
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile
+    config.assets.initialize_on_precompile = false
   end
 end
