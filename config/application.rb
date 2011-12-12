1b7b80fd14bc08fe3084ae2c301fb2a7d3c64203
Olli Jokinen <olli.jokinen@enemy.fi>
2011-12-12 18:12:33 +0200

    heroku asset test

--- config/application.rb
+++ config/application.rb
@@ -47,5 +47,7 @@ module Lolnas
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile
   end
 end
