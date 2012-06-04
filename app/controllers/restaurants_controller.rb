5147664190fe115c489d1eb4fbe67bb06b12228d
Olli Jokinen <olli.jokinen@enemy.fi>
2012-06-04 13:59:19 +0300

    save latitude&longitude parameters to session cookie

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,8 +2,11 @@ class RestaurantsController < ApplicationController
 
   def index
     # defaults
-    @latitude = params[:latitude].nil? ? 60.172389 : params[:latitude].to_f
-    @longitude = params[:longitude].nil? ? 24.947516 : params[:longitude].to_f
+    @latitude = (session[:latitude].nil? or session[:latitude].to_f == 0) ? 60.172389 : session[:latitude].to_f
+    @lontigude = (session[:longitude].nil? or session[:longitude].to_f == 0) ? 24.947516 : session[:longitude].to_f
+
+    @latitude = session[:latitude] = params[:latitude].to_f unless params[:latitude].nil? or params[:latitude].to_f == 0
+    @longitude = session[:longitude] = params[:longitude].to_f unless params[:longitude].nil? or params[:longitude].to_f == 0
 
     # sort by distance
     @restaurants = Restaurant.sort_by_distance(Restaurant.all, @latitude, @longitude)
