571426175a800ea929d3e87c5b7f5089e6c7aa27
Olli Jokinen <olli.jokinen@enemy.fi>
2012-06-06 11:52:20 +0300

    use location-cookies instead of session

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,30 @@
 class RestaurantsController < ApplicationController
 
   def index
-    # defaults
-    @latitude = (session[:latitude].nil? or session[:latitude].to_f == 0) ? 60.172389 : session[:latitude].to_f
-    @lontigude = (session[:longitude].nil? or session[:longitude].to_f == 0) ? 24.947516 : session[:longitude].to_f
-
-    @latitude = session[:latitude] = params[:latitude].to_f unless params[:latitude].nil? or params[:latitude].to_f == 0
-    @longitude = session[:longitude] = params[:longitude].to_f unless params[:longitude].nil? or params[:longitude].to_f == 0
+    @latitude, @longitude = user_location
 
     # sort by distance
     @restaurants = Restaurant.sort_by_distance(Restaurant.all, @latitude, @longitude)
   end
+
+  private
+
+  def user_location
+    # defaults
+    latitude = 60.172389
+    longitude = 24.947516
+
+    if !params[:latitude].nil? and !params[:longitude].nil?
+      latitude = params[:latitude].to_f
+      longitude = params[:longitude].to_f
+      cookies[:location] = { "latitude" => latitude, "longitude" => longitude }.to_json
+    elsif !cookies[:location].nil?
+      last_known_location = JSON.parse(cookies[:location])
+      latitude = last_known_location["latitude"]
+      longitude = last_known_location["longitude"]
+    end
+
+    return latitude, longitude
+  end
+
 end
