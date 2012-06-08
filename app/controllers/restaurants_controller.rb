cdc20f48a34de24e75b7d3834e8a698e566b51a7
Olli Jokinen <olli.jokinen@cs.helsinki.fi>
2012-06-09 00:52:48 +0300

    geocoder gem & geocode address-parameter

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,18 @@
 class RestaurantsController < ApplicationController
 
   def index
-    # defaults
-    Location.latitude = params[:latitude].nil? ? 60.172389 : params[:latitude].to_f
-    Location.longitude = params[:longitude].nil? ? 24.947516 : params[:longitude].to_f
+    if params[:address]
+      location = Geocoder.search("#{params[:address]}")
+      if location.count > 0
+        Location.latitude, Location.longitude = location.first.coordinates
+      end
+    end
+
+    unless Location.latitude
+      # defaults
+      Location.latitude = params[:latitude].nil? ? 60.172389 : params[:latitude].to_f
+      Location.longitude = params[:longitude].nil? ? 24.947516 : params[:longitude].to_f
+    end
 
     # sort by distance
     @restaurants = Restaurant.sort_by_distance(Restaurant.all,
