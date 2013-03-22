b82068b4a92ef6afc0262eab16d021086d4bbdb4
Olli Jokinen <olli.jokinen@cs.helsinki.fi>
2013-03-22 14:22:40 +0200

    check if secret_token env variable exists

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-if Rails.env.production?
+if Rails.env.production? and ENV['SECRET_TOKEN']
   Lolnas::Application.config.secret_token = ENV['SECRET_TOKEN']
 else
   Lolnas::Application.config.secret_token = '27c85c809c673f5cc0d816f7f95b45eb2f6d089f79f0bd9374d2532f08d0a9ab9abfc6d33cc63238c5049b3c0c0768016c6126abdcdb9cee372feb191181f4e9'
